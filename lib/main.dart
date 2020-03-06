5dbeed619713b2b26586a0a06f3302af21cb34b3
vi.vu001 <viet.vu001@stud.fh-dortmund.de>
2020-03-06 15:55:09 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+          centerTitle: true,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(child: FlatButton(
+            onPressed: () {
+              print('Hello');
+              Image.asset('images/dice1.png');
+            },
+            child: Image.asset('images/dice1.png')
+          )),
+          Expanded(child: FlatButton(child: Image.asset('images/dice2.png')))
+        ],
+      ),
+    );
+  }
+}
